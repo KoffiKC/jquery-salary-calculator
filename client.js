34660f042beed6cc796edf5ec06358bf8ad09c7e
Koffi Kittleson <koffi.k.collins@gmail.com>
2022-01-30 17:49:14 -0600

    when an employee is deleted, this will update the DOMs total monly cost asweel as the table

--- client.js
+++ client.js
@@ -10,33 +10,43 @@ function onReady() {
 }
 
 function removeEmployee() {
-    // this tests this
-    console.log($(this));
-    $(this).parent().parent().empty();
-
-    let that = $(this).parent()
-    console.log(that);
+    // target child containing salary data
+    let salary = $(this).parent().parent().children('#salary').text()
+    let idNum = $(this).parent().parent().children('#id-num').text()
     
+    for (let i in employeeSalaries) {
+        if (salary == employeeSalaries[i].sal && idNum == employeeSalaries[i].id) {
+            employeeSalaries[i].sal = 0;
+            console.log(employeeSalaries);
+        }
+        
+    }
+    // remove
+    $(this).parent().parent().remove();
+
+    updateMonthlyTotal();
 }
 
 function updateMonthlyTotal() {
     let total = 0; // initialize counter variable
     
     for(let salary of employeeSalaries){ // loop over employee salary array
-        total += Math.round(salary/12); // add each employee monthly cost to counter
+        total += Math.round(salary.sal/12); // add each employee monthly cost to counter
     }
     //update total monthly on DOM
     $('#mon-total').text(total);
 
     // update monthly total element when exceeding amount
-    if (total > 200000){
+    if (total > 20000){
         $('p').addClass("red")
+    } else if (total < 20000) {
+        $('p').removeClass("red")
     }
 }
 
 function displayEmployee(nameF, nameL, idNum, title, annSalary) {
     // append array data into
-    $('#table-grossness').append(`<tr><td>${nameF}</td><td>${nameL}</td><td>${idNum}</td><td>${title}</td><td>${annSalary}</td><td><button id="delete-button">Delete</button></td></tr>`)    
+    $('#table-grossness').append(`<tr><td>${nameF}</td><td>${nameL}</td><td id="id-num">${idNum}</td><td>${title}</td><td id="salary" >${annSalary}</td><td><button id="delete-button">Delete</button></td></tr>`)    
 
     // empty input fields
     $('#first-name-in').val(""); 
@@ -46,18 +56,14 @@ function displayEmployee(nameF, nameL, idNum, title, annSalary) {
     $('#ann-sal-in').val("");
 }
 
-// function addEmployee(nameF, nameL, idNum, title, annSalary) {
-//         const employee = {
-//         firstName: nameF,
-//         lastName: nameL,
-//         id: idNum,
-//         title: title,
-//         salary: annSalary,
-//         monSalary: annSalary/12,
-//         }
-//         // adds object to employees array
-//         employees.push(employee);
-// }
+function addEmployeeInfo(idNum, annSalary) {
+        const employee = {
+        id: idNum,
+        sal: annSalary,
+        }
+        // adds object to employees array
+        employeeSalaries.push(employee);
+}
 
 function submitForm() {
     console.log('Hey it works huh cool');
@@ -69,7 +75,7 @@ function submitForm() {
     let annSalary = $('#ann-sal-in').val();
 
     // add salary to array for later manipulaton
-    employeeSalaries.push(annSalary);
+    addEmployeeInfo(idNum, annSalary);
     // addEmployee(nameF, nameL, idNum, title, annSalary); // add employee to employees array
     // console.log(employees); // testing
     // append input values onto the DOM
